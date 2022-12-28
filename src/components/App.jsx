653f7853f9e8fa3b4df79ae6d796cee4b53e4cfc
Hurduz Oksana <hurduzoksana@gmail.com>
2022-12-28 10:36:11 +0200

    Add functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,63 @@
-import Phonebook from "./Phonebook";
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 20,
-        color: '#010101'
-      }}
-    >
-      <Phonebook/>
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: uuidv4(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: uuidv4(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: uuidv4(), name: 'Eden Clements', number: '645-17-79' },
+      { id: uuidv4(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    if (
+      this.state.contacts.map(contact => contact.name).includes(newContact.name)
+    ) {
+      alert(`${newContact.name} is already in contacts`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  findContact = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  getVisibleContacts = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.findContact} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </>
+    );
+  }
+}
